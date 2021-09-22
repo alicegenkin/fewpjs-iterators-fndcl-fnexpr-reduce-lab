6e1bc5e4f700a345f0bb2ef888934ca6fcad6807
Alice Genkin <agenkin1@gmail.com>
2021-09-22 12:01:33 -0400

    commit tree 9b77960024d0f7c8b6cddf76d186bcdd47d13fc8

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+//incomingStudents.reduce(function(houses, student) { houses[sortingHat.assign(student)].push(student)}, hogwartsHouses)
+
+const totalBatteries = batteryBatches.reduce(function(total,batches) {
+  return batches + total
+}
+)
